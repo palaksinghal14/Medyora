fac1262013b5704e8f617039acb3c6dabac6631e
palaksinghal14 <palaksinghal148@gmail.com>
2026-01-19 11:04:48 +0530

    updated navigation

--- app/src/main/java/com/example/medyora/navigation/MainRoutes.kt
+++ app/src/main/java/com/example/medyora/navigation/MainRoutes.kt
@@ -43,7 +43,9 @@ fun MainNavGraph(
         }
 
         composable(MainRoutes.SETTINGS) {
-            SettingsScreen()
+            SettingsScreen(
+                { navController.navigate("signup") }
+            )
         }
 
         composable(MainRoutes.SYMPTOM) {
