293c484a96809abab3ab2472a63d461ba0a37e72
palaksinghal14 <palaksinghal148@gmail.com>
2026-01-19 10:40:21 +0530

    added dialog box in settings

--- app/src/main/java/com/example/medyora/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/medyora/screens/SettingsScreen.kt
@@ -32,16 +32,22 @@ import androidx.compose.material.icons.outlined.ContactSupport
 import androidx.compose.material.icons.outlined.DarkMode
 import androidx.compose.material.icons.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -81,6 +87,9 @@ data class SettingsItem(
 
 @Composable
 fun SettingsScreen() {
+
+    var showDialogLogOut by remember { mutableStateOf(false) }
+    var showDialogDelete by remember { mutableStateOf(false) }
 
     val settingsGroups = listOf(
         SettingsGroup(
@@ -214,7 +223,9 @@ fun SettingsScreen() {
                         Spacer(modifier = Modifier.height(12.dp))
 
                         OutlinedButton(
-                            onClick = {},
+                            onClick = {
+                                showDialogDelete=true
+                            },
                             modifier = Modifier
                                 .fillMaxWidth(),
                             shape = RoundedCornerShape(10.dp),
@@ -251,7 +262,9 @@ fun SettingsScreen() {
             // signout button
             item {
                 OutlinedButton(
-                    onClick = {},
+                    onClick = {
+                        showDialogLogOut=true
+                    },
                     modifier = Modifier
                         .fillMaxWidth(),
                     shape = RoundedCornerShape(10.dp),
@@ -282,6 +295,7 @@ fun SettingsScreen() {
                 }
             }
 
+
             //details
             item {
                 Column(
@@ -302,6 +316,26 @@ fun SettingsScreen() {
 
 
         }
+    }
+
+    if (showDialogLogOut==true){
+        showDiaglog(
+            onDismiss = {showDialogLogOut=false },
+            onConfirm = {},
+            description = "Are you sure you want to sign out",
+            title = "SIGN OUT"
+
+
+        )
+    }
+    if (showDialogDelete==true){
+        showDiaglog(
+            onDismiss = {showDialogDelete=false},
+            onConfirm = {},
+            description = "Are you sure you want to delete your data",
+            title = "DELETE ALL DATA"
+
+        )
     }
 
 }
@@ -385,6 +419,57 @@ fun SettingsItemRow(item: SettingsItem) {
             tint = Gray500
         )
     }
+}
+
+
+@Composable
+fun showDiaglog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    description: String,
+    title: String
+){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            OutlinedButton(
+                onClick = onConfirm,
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, Gray900),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = White,
+                    contentColor = Gray900
+                )
+            ) {
+                Text(text = "Confirm")
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = onDismiss,
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, Gray900),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = White,
+                    contentColor = Gray900
+                )
+            ) {
+                Text(text ="Cancel" )
+            }
+        },
+        text = {Text(
+            text = description,
+            fontSize = 14.sp,
+            color = Gray900
+        )},
+        title = {Text(
+            text = title,
+            fontSize = 16.sp,
+            color = Gray900
+        )},
+        containerColor = White,
+
+    )
 }
 
 
