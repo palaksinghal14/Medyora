475267a55d60a2034b7a78d43b3cb4b6ab290672
palaksinghal14 <palaksinghal148@gmail.com>
2026-05-05 04:50:31 +0530

    PLACES API retrofit setup for doctor feature

--- app/src/main/java/com/example/medyora/di/NetworkModule.kt
+++ app/src/main/java/com/example/medyora/di/NetworkModule.kt
@@ -1,11 +1,16 @@
 package com.example.medyora.di
 
+import com.example.medyora.Repository.PlacesApiService
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -20,5 +25,22 @@ object NetworkModule
     @Provides
     @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore= FirebaseFirestore.getInstance()
+
+    @Provides @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://maps.googleapis.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+                .build()
+        )
+        .build()
+
+    @Provides @Singleton
+    fun providePlacesApi(retrofit: Retrofit): PlacesApiService =
+        retrofit.create(PlacesApiService::class.java)
 
 }
