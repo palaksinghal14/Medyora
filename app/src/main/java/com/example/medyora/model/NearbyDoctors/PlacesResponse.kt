82f87b3bc115bdbd6692c708bf8efeb811cede87
palaksinghal14 <palaksinghal148@gmail.com>
2026-05-05 04:46:09 +0530

    added model classes for doctor feature

---
+++ app/src/main/java/com/example/medyora/model/NearbyDoctors/PlacesResponse.kt
@@ -0,0 +1,26 @@
+package com.example.medyora.model.NearbyDoctors
+
+import com.google.gson.annotations.SerializedName
+
+// data/remote/dto/PlacesResponse.kt
+data class PlacesResponse(
+    val results: List<PlaceResult>,
+    val status: String
+)
+
+data class PlaceResult(
+    @SerializedName("place_id")   val placeId: String,
+    @SerializedName("name")       val name: String,
+    @SerializedName("vicinity")   val vicinity: String,
+    @SerializedName("rating")     val rating: Float?,
+    @SerializedName("geometry")   val geometry: Geometry,
+    @SerializedName("opening_hours") val openingHours: OpeningHours?
+)
+
+data class Geometry(val location: LatLngDto)
+
+data class LatLngDto(val lat: Double, val lng: Double)
+
+data class OpeningHours(
+    @SerializedName("open_now") val openNow: Boolean?
+)
